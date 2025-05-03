86d54f54bb5f42ff305b4aae74cd45a278c6f116
Aldahir Alonso <aldahir0603@hotmail.com>
2025-05-03 17:20:12 -0600

    Primer ejercicio

---
+++ sintaxis-basica.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Hola, estoy programando en PHP. \n";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+// echo "Mi nombre es " . $nombre . " " . $apellido;
+
+// echo "Mi nombre es $nombre $apellido";
+
+// echo "El resultado de 4 x 5 es " . (4 * 5);
+
+$personas = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+
+// var_dump($personas); *permite ver todo el contenido de la variable $personas.
+// print_r($personas); *permite ver todo el contenido de $personas pero de manera mas simple.
+
+// Esto es un comentario
+# Esto es otro comentario
+
+/* 
+Cualquier cosa
+Abajo puedo seguir escribiendo mas cosas
+*/
+
+echo "\n";
