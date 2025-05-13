63d4be34716ffed6848b6599d19e767679d6d2a3
Aldahir Alonso <aldahir0603@hotmail.com>
2025-05-12 21:10:33 -0600

    Resto de ejercicios

---
+++ otros-operadores.php
@@ -0,0 +1,17 @@
+<?php
+
+// Operador de asignacion
+/* $edad_de_vivi = ($edad_de_bonis = 18) + 5;
+
+echo "La edad de bonis es = $edad_de_bonis\n";
+echo "La edad de vivi es = $edad_de_vivi"; */
+
+// Operador de incremento
+
+$contador = 1;
+
+// $contador = $contador + 1;
+// $contador += $contador + 1;
+$contador++;
+
+echo $contador;
---
+++ reto.php
@@ -0,0 +1,9 @@
+<?php
+
+$horas = readline("Ingresa el tiempo en horas: ");
+$minutos = readline("Ingresa el tiempo en minutos: ");
+$segundos = readline("Ingresa el tiempo en segundos: ");
+
+$tiempo_en_segundos * ($horas * 60 * 60) + ($minutos * 60) +
+$segundos;
+
