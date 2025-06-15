78eaf35ca945f5974136341ac603b40530793d59
Aldahir Alonso <aldahir0603@hotmail.com>
2025-06-15 17:58:59 -0600

    Ejercicios Dinamicos con PHP y HTML

---
+++ php-con-html/01_variables.php
@@ -0,0 +1,17 @@
+<?php
+$course = "Curso profesional de PHP y Laravel";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $course; ?></title>
+</head>
+<body>
+    <h1>Bienvenido al <?= $course; ?></h1>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores distinctio facilis dolorum eos possimus harum libero neque temporibus sed at fuga, minima ipsa suscipit adipisci illum voluptas eum? Quasi, unde?</p>
+</body>
+</html>
