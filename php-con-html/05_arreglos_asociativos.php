0f482523dbde02580e2e2d1f4d5a41d216045c61
Aldahir Alonso <aldahir0603@hotmail.com>
2025-06-17 11:52:30 -0600

    arreglos asociativos

--- php-con-html/05_arreglos_asociativos.php
+++ php-con-html/05_arreglos_asociativos.php
@@ -1,14 +1,16 @@
 <?php
-$course = "Curso profesional de PHP y Laravel";
-
-$tags = [
-    "PHP", // 0
-    "Laravel", // 1
-    "JavaScript", // 2
-    "HTML", // 3
-    "CSS", // 4
-    "MySQL", // 5
-    "Git", // 6
+$course = [
+    'title' => "Curso profesional de PHP y Laravel",
+    'subtitle' => "Desarrolla aplicaciones web con PHP y Laravel",
+    'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    'tags' => [
+        'PHP',
+        'Laravel',
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'MySQL',
+    ],
 ];
 ?>
 
@@ -17,18 +19,20 @@ $tags = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $course; ?></title>
+    <title><?= $course['title'] ?></title>
 </head>
 <body>
-    <h1>Bienvenido al <?= $course; ?></h1>
+    <h1>Bienvenido al <?= $course['title'] ?></h1>
 
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores distinctio facilis dolorum eos possimus harum libero neque temporibus sed at fuga, minima ipsa suscipit adipisci illum voluptas eum? Quasi, unde?</p>
+    <h2><?= $course['subtitle'] ?></h2>
+
+    <p><?= $course['description'] ?></p>
 
     <p>
         <strong>Etiquetas:</strong>
         <ul>
-            <?php foreach ($tags as $tag): ?>
-                <li><?= $tag; ?></li>
+            <?php foreach ($course['tags'] as $tag): ?>
+                <li><?= $tag ?></li>
             <?php endforeach; ?>
         </ul>
     </p>
