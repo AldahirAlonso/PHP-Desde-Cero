049fd7f075b6cb6f5604c131858cdac0334cb285
Aldahir Alonso <aldahir0603@hotmail.com>
2025-06-19 10:23:40 -0600

    Enumeraciones - Curso gratuito o de pago

---
+++ php-con-html/CourseType.php
@@ -0,0 +1,14 @@
+<?php
+
+enum CourseType: string {
+    case FREE = "free";
+    case PAID = "paid";
+
+    public function label(): string 
+    {
+        return match($this) {
+            self::FREE => "Curso gratuito",
+            self::PAID => "Curso de pago",
+        };
+    }
+}
